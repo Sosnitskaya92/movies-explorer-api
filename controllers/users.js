0a5e1d94471ab82633dabfc1c71e3a5589e16cdc
Angelina <Sosnitskaya92@mail.ru>
2022-07-21 22:42:58 +0300

    fix: SameSite

--- controllers/users.js
+++ controllers/users.js
@@ -72,7 +72,7 @@ module.exports.login = (req, res, next) => {
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: true,
+        sameSite: false,
       }).send({ message: 'Вы успешно вошли.' });
     })
     .catch(() => {
